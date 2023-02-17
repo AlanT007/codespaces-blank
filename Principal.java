c9caf630b81c7f84e83e57177c90bc35695e2e45
AlanT007 <125695125+AlanT007@users.noreply.github.com>
2023-02-17 20:10:19 +0000

    first commit

---
+++ Principal.java
@@ -0,0 +1,24 @@
+/*
+*Principal.Java
+*Mi primer programa en java 
+*@autor Alan Torres Perez(perrez657@gmail.com)
+*@date 17/02/2023
+*GPL v3 
+*/
+
+ public class Principal {
+     /** 
+     *funcion principal
+     *@param arg parametros de entrada del programa 
+     */
+     public static void  main(String[] args){
+       System.out.println(getMassage());
+    }
+    /*
+    *devuelve el mensaje de bienvenida
+    *@retun el mensaje
+    */
+   public static String getMassage(){ 
+  return "Hola a todos";
+     }
+}
